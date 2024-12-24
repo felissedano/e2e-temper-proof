969b101008431ffd9efe1e17a304833bb7df4960
felissedano <felis@felissedano.com>
2024-12-23 23:47:20 -0500

    implement data tampering discover and recover mechanism

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,47 @@
+import { get } from "http";
 import React, { useEffect, useState } from "react";
 
 const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [hash, setHash] = useState<string>();
+  const [salt, setSalt] = useState<string>();
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [showVerificationWindow, setShowVerificationWindow] = useState<boolean>(false);
+  const [isTampered, setIsTampered] = useState<boolean>(false);
+  const [showHackerWindow, setShowHackerWindow] = useState<boolean>(false);
+
 
   useEffect(() => {
     getData();
   }, []);
 
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    if (hasError) {
+      timeoutId = setTimeout(() => setHasError(false), 2000);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [hasError]);
+
   const getData = async () => {
     const response = await fetch(API_URL);
     const { data } = await response.json();
-    setData(data);
+    console.log(data.value);
+    setData(data.value);
+    setHash(data.saltedHash);
   };
 
   const updateData = async () => {
+    const isValidated = validateForm();
+    if (!isValidated) {
+      setHasError(true);
+      return;
+    }
     await fetch(API_URL, {
       method: "POST",
-      body: JSON.stringify({ data }),
+      body: JSON.stringify({ data, salt }),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
@@ -29,7 +52,48 @@ function App() {
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    const response = await fetch(API_URL + "/verify", {
+      method: "POST",
+      body: JSON.stringify({ salt }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { isTampered } = await response.json();
+    console.log(isTampered);
+
+    if (isTampered) {
+      setIsTampered(true);
+      setShowVerificationWindow(true);
+    } else {
+      setIsTampered(false);
+      setShowVerificationWindow(true);
+    }
+  };
+
+  const recoverData = async () => {
+    const response = await fetch(API_URL + "/recover-data", {
+      method: "POST",
+      body: JSON.stringify({ salt }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    await getData();
+
+    setShowVerificationWindow(false);
+  }
+
+  const validateForm = () => {
+    if (!data || !salt) {
+      return false;
+    } else {
+      return true;
+    }
   };
 
   return (
@@ -45,14 +109,32 @@ function App() {
         flexDirection: "column",
         gap: "20px",
         fontSize: "30px",
+        borderColor: "black",
       }}
     >
+      {showVerificationWindow && <VerificationWindow isTampered={isTampered} recoverData={recoverData} closeVerificationWindow={() => setShowVerificationWindow(false)} />}
+
+      {showHackerWindow && <HackerWindow closeHackerWindow={() => setShowHackerWindow(false)} getData={getData} />}
+
       <div>Saved Data</div>
       <input
         style={{ fontSize: "30px" }}
         type="text"
         value={data}
         onChange={(e) => setData(e.target.value)}
+      />
+
+      <div>Saved Hash</div>
+      <span>{hash}</span>
+
+      <br />
+
+      <div>Your Salt (Please use the same salt when updating and verifying the data)</div>
+      <input
+        style={{ fontSize: "30px" }}
+        type="text"
+        value={salt}
+        onChange={(e) => setSalt(e.target.value)}
       />
 
       <div style={{ display: "flex", gap: "10px" }}>
@@ -63,8 +145,119 @@ function App() {
           Verify Data
         </button>
       </div>
+
+      <div>
+        <button style={{ fontSize: "20px", color: "red", backgroundColor: "black" }} onClick={() => setShowHackerWindow(true)}>
+          <b>Hacker Zone: Click to Tamper Data</b>
+        </button>
+      </div>
+
+      {hasError && <div style={{ color: "red", fontSize: "20px" }}>One or more fields are missing</div>}
     </div>
   );
+}
+
+function VerificationWindow({ isTampered, recoverData, closeVerificationWindow }: { isTampered: boolean, recoverData: () => void, closeVerificationWindow: () => void }) {
+  return (
+    <div style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "rgba(0,0,0,0.5)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <div style={{
+        backgroundColor: "white",
+        padding: "20px",
+        borderRadius: "10px",
+        display: "flex",
+        gap: "10px",
+        flexDirection: "column",
+      }}>
+        <div style={{ fontSize: "30px" }}>{isTampered ? "Data has been tampered" : "Data has not been tampered"}</div>
+        <br />
+        {isTampered && <button style={{ fontSize: "20px" }} onClick={recoverData}>Reset Data</button>}
+        <button style={{ fontSize: "20px" }} onClick={closeVerificationWindow}>Close</button>
+      </div>
+    </div>
+  )
+}
+
+function HackerWindow({ closeHackerWindow, getData }: { closeHackerWindow: () => void, getData: () => void }) {
+  const [data, setData] = useState("");
+  const [salt, setSalt] = useState("");
+
+  const tamperData = async () => {
+    console.log(data, salt);
+    const response = await fetch(API_URL + "/malicious/tamper-data", {
+      method: "POST",
+      body: JSON.stringify({ data, salt }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { success } = await response.json();
+
+    if (success) {
+      getData();
+      closeHackerWindow();
+    }
+  }
+
+  return (
+    <div style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "rgba(0,0,0,0.5)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <div style={{
+        backgroundColor: "white",
+        padding: "20px",
+        borderRadius: "10px",
+        display: "flex",
+        gap: "10px",
+        flexDirection: "column",
+      }}>
+        <div style={{
+          fontSize: "30px",
+          backgroundColor: "black",
+          color: "red",
+          textAlign: "center",
+
+        }}>Hacker Zone</div>
+        <br />
+        <div>Set the new data</div>
+        <input
+          style={{ fontSize: "30px" }}
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+        <div>(optional) Add custom salt (if empty then hash will stay the same)</div>
+        <input
+          style={{ fontSize: "30px" }}
+          type="text"
+          value={salt}
+          onChange={(e) => setSalt(e.target.value)}
+        />
+
+        <button style={{ fontSize: "20px" }} onClick={tamperData}>Tamper Data</button>
+        <button style={{ fontSize: "20px" }} onClick={closeHackerWindow}>Close</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,73 @@
 import express from "express";
 import cors from "cors";
+import * as crypto from "crypto";
+import e from "express";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+
+//The database schema
+type Schema = {
+  value: string;
+  saltedHash: string;
+};
+
+//Create databse with default value
+const database: { data: Schema } = { data: {
+  value: "Hello World",
+  saltedHash: "NA",
+}
+};
+
+// the log will record every single operation executaed in the database
+type Log = {
+  oldValue: Schema;
+  newValue: Schema;
+}
+
+//append only logger
+ class Logger {
+  logs: Log[] = [];
+  append(oldValue: Schema, newValue: Schema) {
+    console.log("appending", oldValue, newValue);
+    this.logs.push({ oldValue: oldValue, newValue: newValue });
+  }
+
+  retrieve() {
+    return this.logs;
+  }
+
+}
+
+const logger = new Logger();
+
+// create the hash base on the updated value and user provided salt
+const generateHash = (value: string, salt: string): string => {
+  return crypto.createHash("sha256").update(value).update(salt).digest("hex");
+}
+
+// take user provided salt and stored value to verify if matches the stored hash
+const isSameHash = (storedValue: string, salt: string, storedHash: string): boolean => {
+  console.log(generateHash(storedValue, salt));
+  console.log(storedHash);
+  return generateHash(storedValue, salt) === storedHash;
+}
+
+// record the activity and update the database
+const updateDatabase = (value: string, saltedHash: string) => {
+  logger.append({
+    value: database.data.value, saltedHash: database.data.saltedHash
+  }, 
+  {
+    value,
+    saltedHash
+  });
+
+  database.data.value = value;
+  database.data.saltedHash = saltedHash;
+}
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,9 +78,74 @@ app.get("/", (req, res) => {
   res.json(database);
 });
 
+// update the database using the user provided value and salt
 app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+  const value = req.body.data;
+  const salt = req.body.salt;
+  const saltedHash = generateHash(value, salt);
+
+  updateDatabase(value, saltedHash);
+
+  console.log("Logs:\n", logger.retrieve());
+  console.log("Database:\n", database);
+
+  return res.json(database);
+});
+
+// user inputs the salt they used and check if hash matches, if not, then user data has been tampered
+app.post("/verify", (req, res) => {
+  const salt = req.body.salt;
+  const storedValue = database.data.value;
+  const storedHash = database.data.saltedHash;
+
+  if (!isSameHash(storedValue, salt, storedHash)) {
+    console.log("Data has been tampered");
+    return res.json({ isTampered: true });
+  } else {
+    console.log("Data has not been tampered");
+    return res.json({ isTampered: false });
+  }
+  
+});
+
+// find the latest data that matches the user provided salt, if found, update the databse
+app.post("/recover-data", (req, res) => {
+  const salt = req.body.salt;
+  const logs = logger.retrieve();
+
+  for (let i = logs.length - 1; i >= 0; i--) {
+    const log = logs[i];
+    const isLastKnownGoodValue = isSameHash(log.oldValue.value, salt, log.oldValue.saltedHash);
+    if (isLastKnownGoodValue) {
+      console.log("Recovered data:");
+      console.log(log.oldValue.value, log.oldValue.saltedHash);
+      console.log("Updating databse")
+      updateDatabase(log.oldValue.value, log.oldValue.saltedHash);
+      return res.json({
+        success: true,
+        value: log.newValue.value,
+      });
+    }
+  }
+
+  return res.json({
+    success: false
+  })
+
+});
+
+//The route that mock hacker's data tampering attempts.
+app.post("/malicious/tamper-data", (req, res) => {
+  console.log("Hacker tampering the data");
+  const value = req.body.data;
+  const salt = req.body.salt;
+  const saltedHash = salt ? generateHash(database.data.value, salt) : database.data.saltedHash;
+  console.log(value,saltedHash);
+  updateDatabase(value, saltedHash);
+
+  return res.json({
+    success: true
+  });
 });
 
 app.listen(PORT, () => {
